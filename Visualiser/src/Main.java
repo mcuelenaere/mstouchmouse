a7883274a59f3fe6a94544491f1944a1b4567c04
Maurus Cuelenaere <mcuelenaere@gmail.com>
2011-11-17 00:23:56 +0100

    .. and update Java app

--- Visualiser/src/Main.java
+++ Visualiser/src/Main.java
@@ -6,7 +6,6 @@
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.io.BufferedInputStream;
@@ -15,56 +14,154 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicReference;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.SwingUtilities;
 
 public class Main extends JPanel {
-	private AtomicReference<int[]> data;
+	private static class DataParser {
+		private int[][] data;
+		private int lastTime;
+		private int x, y;
+		private boolean advanceFlag;
+
+		public DataParser() {
+			data = new int[15][13];
+			reset();
+		}
+
+		/*
+		 *    0 1 2 3 4 5 6 7 8 9 A B C D E F
+		 *  0       * * * * * * * * * *
+		 *  1     * * * * * * * * * * * *
+		 *  2   * * * * * * * * * * * * * *
+		 *  3   * * * * * * * * * * * * * *
+		 *  4 * * * * * * * * * * * * * * * *
+		 *  5 * * * * * * * * * * * * * * * *
+		 *  6 * * * * * * * * * * * * * * * *
+		 *  7 * * * * * * * * * * * * * * * *
+		 *  8 * * * * * * * * * * * * * * * *
+		 *  9 * * * * * * * * * * * * * * * *
+		 *  A * * * * * * * * * * * * * * * *
+		 *  B * * * * * * * * * * * * * * * *
+		 *  C * * * * * * * * * * * * * * * *
+		 *  D * * * * * * * * * * * * * * * *
+		 */
+		public void advance() {
+		    x++;
+
+		    int max, nextMin;
+		    switch (y) {
+		        case 0:
+		            max = 11;
+		            nextMin = 2;
+		        break;
+		        case 1:
+		            max = 12;
+		            nextMin = 1;
+		        break;
+		        case 2:
+		            max = 13;
+		            nextMin = 1;
+		        break;
+		        case 3:
+		            max = 13;
+		            nextMin = 0;
+		        break;
+		        default:
+		            max = 14;
+		            nextMin = 0;
+		        break;
+		    }
+
+		    if (x > max) {
+		        y++;
+		        x = nextMin;
+		    }
+		}
+
+		private void parseNibble(int b) {
+		    if (advanceFlag) {
+		        advanceFlag = false;
+		        for (int i = -3; i < b; i++)
+		            advance();
+		    } else {
+		        if (b == 0xF) {
+		            advanceFlag = true;
+		        } else {
+		        	if (y < 13)
+		        		data[x][y] = (0xFF * b) / 0xF;
+		            advance();
+		        }
+		    }
+		}
+
+		public void parseData(int buffer[]) {
+			if (buffer[0] != 0x27 || buffer[5] != 0x51)
+				return;
+
+			int dataLength = buffer[1];
+			int curTime = buffer[6];
+
+			if (lastTime != curTime) {
+				reset();
+				lastTime = curTime;
+			}
+
+		    for (int i=7; i < 7 + dataLength - 1; i++) {
+		    	parseNibble(buffer[i] & 0xF);
+		    	parseNibble(buffer[i] >> 4);
+		    }
+		}
+
+		private void reset() {
+			for (int[] line : data)
+				Arrays.fill(line, 0);
+			x = 3;
+			y = 0;
+			advanceFlag = false;
+		}
+
+		public int[][] getData() {
+			return data;
+		}
+	}
+
+	private DataParser parser = new DataParser();
 
 	public Main(final File hidraw)
 	{
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
-		data = new AtomicReference<int[]>(new int[] {0xff, 0xff, 0xff}); /* dummy data */
-
-		String[][] tableData = new String[32][3];
+		String[][] tableData = new String[32][2];
 		for (int i=0; i < 32; i++) {
 			tableData[i][0] = String.valueOf(i);
 			tableData[i][1] = "0x00";
-			tableData[i][2] = "0x00";
 		}
 
 		tableData[0][0] = "hid_id";
-		tableData[1][0] = "group_id/pressure";
-		tableData[2][0] = tableData[3][0] = tableData[4][0] = tableData[5][0] = "header";
+		tableData[1][0] = "length";
+		tableData[2][0] = tableData[3][0] = tableData[4][0] = "unknown";
+		tableData[5][0] = "footer";
 		tableData[6][0] = "timestamp";
 
-		final JTable table = new JTable(tableData, new String[] {"description", "values", "values2"});
+		final JTable table = new JTable(tableData, new String[] {"description", "values"});
 		Box b = Box.createVerticalBox();
 		b.setBorder(BorderFactory.createTitledBorder("Raw data"));
 		b.add(table.getTableHeader());
 		b.add(table);
 
-		Vis v = new Vis();
+		Visualizer v = new Visualizer();
 		v.setBorder(BorderFactory.createTitledBorder("Visualizer"));
-
-		final JLabel l = new JLabel();
-		l.setText("<filler><filler><filler><filler>");
-		l.setFont(Font.getFont(Font.MONOSPACED));
-		l.setBorder(BorderFactory.createTitledBorder("Description"));
 
 		add(v);
 		add(b);
-		add(l);
 
 		System.out.println(hidraw);
 		new Thread(new Runnable() {
@@ -82,11 +179,9 @@ public class Main extends JPanel {
 								buf[i] = r.readUnsignedByte();
 
 							for (int i = 0; i < buf.length; i++)
-								table.setValueAt(String.format("0x%02x", buf[i]), i, buf[1] < 0x0f ? 2 : 1);
+								table.setValueAt(String.format("0x%02x", buf[i]), i, 1);
 
-							parseData(l, buf);
-
-							data.set(Arrays.copyOfRange(buf, 7, 32));
+							parser.parseData(buf);
 							repaint();
 						} else if (bit == 0x21) {
 							int b1 = r.readUnsignedByte(), b2 = r.readUnsignedByte();
@@ -102,60 +197,24 @@ public class Main extends JPanel {
 		}).start();
 	}
 
-	private String parseMousePart(int val) {
-		int high = val & 0xF0;
-		int low = val & 0x0F;
-		if (high != 0x20 && high != 0x00)
-			return "N/A";
-
-		switch (low) {
-		case 0x01:
-			return "left";
-		case 0x02:
-			return "right";
-		default:
-			return "N/A";
-		}
-	}
-
-	private void parseData(JLabel l, int buf[]) {
-		int endPos = 31;
-		for (int i=31; i >= 7; i--) {
-			if (buf[i] != 0x00) {
-				endPos = i;
-				break;
-			}
-		}
-
-		l.setText(String.format("<html>" +
-								"endPos: %d (0x%02x)<br>" +
-								"mouse part: 0x%02x %s<br>" +
-								"2nd timestamp: 0x%02x<br>",
-								endPos, endPos,
-								buf[endPos-1], parseMousePart(buf[endPos-1]),
-								buf[endPos]));
-	}
-
-	private class Vis extends JComponent {
-		public Vis()
-		{
+	private class Visualizer extends JComponent {
+		public Visualizer() {
 			setPreferredSize(new Dimension(400, 400));
 		}
 
 		@Override
 		protected void paintComponent(Graphics g) {
 			Insets insets = getInsets();
-			int width = getWidth() - insets.left - insets.right, height = getHeight() - insets.top - insets.bottom;
-			int x = insets.left, y = insets.top;
-			final int data[] = Main.this.data.get();
-			final int part = height / data.length;
-			for (int i=0; i < data.length; i++) {
-				g.setColor(new Color(0xff - data[i], 0, 0));
-				g.fillRect(x, y + part * i, width, part);
+			int totalWidth = getWidth() - insets.left - insets.right, totalHeight = getHeight() - insets.top - insets.bottom;
+			int left = insets.left, top = insets.top;
 
-				int loc = data[i] * width / (1 << 8);
-				g.setColor(new Color(0, 0, 0xff));
-				g.drawLine(x + loc, y + part * i, x + loc, y + part * (i+1));
+			final int[][] data = parser.getData();
+			int pixWidth = totalWidth / data.length, pixHeight = totalHeight / data[0].length;
+			for (int x=0; x < data.length; x++) {
+				for (int y=0; y < data[0].length; y++) {
+					g.setColor(new Color(data[x][y], 0, 0));
+					g.fillRect(left + x * pixWidth, top + y * pixHeight, pixWidth, pixHeight);
+				}
 			}
 		}
 	}
